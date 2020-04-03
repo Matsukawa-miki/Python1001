686e5a4caac86f2f16c72a3821fc50e7b8e005b0
Matsukawa-miki <63024035+Matsukawa-miki@users.noreply.github.com>
2020-04-03 16:43:48 -0500

    Add files via upload

---
+++ 1lesson.py
@@ -0,0 +1,28 @@
+#Python 1001
+#Lesson 1
+#April 3, 2020
+#Written by Matsukawa
+
+string1= "this is a string"
+
+string2= input("Enter a word:")
+
+#print the string1 and string2
+print(string1)
+print(string2)
+
+#making a list
+music_notes_list= {"C", "D", "E", "F", "G", "A", "B"}
+#print the list
+print(music_notes_list)
+
+#print each element in list on seperate line
+#create a loop
+for note in music_notes_list:
+    print(note)
+
+
+#Notes:
+#1. Try sorting the list
+#2. Append the list
+#3. Create a function to print multiple lists
